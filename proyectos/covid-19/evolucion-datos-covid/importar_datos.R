52aa80e8f3be19ebf313da9f07c95e3f50259606
Carlos Munoz Diaz <cmunoz@elconfidencial.com>
2020-10-08 12:21:40 +0200

    Actualización de formatos - Para provincias en Rebrotes, datos consolidados 3/10

--- proyectos/covid-19/evolucion-datos-covid/importar_datos.R
+++ proyectos/covid-19/evolucion-datos-covid/importar_datos.R
@@ -2,16 +2,17 @@
 library(tidyverse)
 library(lubridate)
 
-setwd('./proyectos/covid-19/evolucion-datos-covid')
+#setwd('./proyectos/covid-19/evolucion-datos-covid')
+#setwd('../evolucion-datos-covid')
 
 infectados <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
 muertes <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")
 recuperados <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv")
 
 
-## reordenar data frames haciéndolos más largos y convertir columna fecha a formato fecha (script: 06/10; fecha último dato: 05/10)
+## reordenar data frames haciéndolos más largos y convertir columna fecha a formato fecha (script: 08/10; fecha último dato: 07/10)
 infectados_long <- infectados %>% 
-    pivot_longer(names_to = "fecha", values_to = "casos", cols = 5:262) %>% # añadir cada día una columna nueva
+    pivot_longer(names_to = "fecha", values_to = "casos", cols = 5:264) %>% # añadir cada día una columna nueva
     mutate(fecha_buena=mdy(fecha), # convertir columna fecha a formato fecha
            region=case_when(
                `Country/Region`=="China" ~ "China",
@@ -19,7 +20,7 @@ infectados_long <- infectados %>%
     select(provincia=`Province/State`, pais=`Country/Region`, region, lat=Lat, long=Long, fecha, fecha_buena, casos)
 
 muertes_long <- muertes %>% 
-    pivot_longer(names_to = "fecha", values_to = "muertes", cols = 5:262) %>% 
+    pivot_longer(names_to = "fecha", values_to = "muertes", cols = 5:264) %>% 
     mutate(fecha_buena=mdy(fecha), # convertir columna fecha a formato fecha
            region=case_when(
                `Country/Region`=="China" ~ "China",
@@ -27,7 +28,7 @@ muertes_long <- muertes %>%
     select(provincia=`Province/State`, pais=`Country/Region`, region, lat=Lat, long=Long, fecha, fecha_buena, muertes)
 
 recuperados_long <- recuperados %>% 
-    pivot_longer(names_to = "fecha", values_to = "recuperados", cols = 5:262) %>% 
+    pivot_longer(names_to = "fecha", values_to = "recuperados", cols = 5:264) %>% 
     mutate(fecha_buena=mdy(fecha), # convertir columna fecha a formato fecha
            region=case_when(
                `Country/Region`=="China" ~ "China",
