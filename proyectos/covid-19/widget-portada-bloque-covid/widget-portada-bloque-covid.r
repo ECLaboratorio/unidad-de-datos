f590546a277f702003f9198d017e79ed8504e8ff
Carlos Munoz Diaz <cmunoz@elconfidencial.com>
2020-11-11 16:42:17 +0100

    Nueva prueba para Widget y Panel

--- proyectos/covid-19/widget-portada-bloque-covid/widget-portada-bloque-covid.r
+++ proyectos/covid-19/widget-portada-bloque-covid/widget-portada-bloque-covid.r
@@ -3,10 +3,10 @@ library(tidyverse)
 library(lubridate)
 
 ## Recogemos todos los CSV de Google Drive
-datosContagiosHistograma <- read.csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQVo0EJNeZWoF4XruYd0BnoHyWxgInurmI3iAWJRZplmYF7_YzGvesj1onZdfY3dbHI6-FRHkuQYXon/pub?gid=618971894&single=true&output=csv')
-datosMuertesHistograma <- read.csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQVo0EJNeZWoF4XruYd0BnoHyWxgInurmI3iAWJRZplmYF7_YzGvesj1onZdfY3dbHI6-FRHkuQYXon/pub?gid=939250240&single=true&output=csv')
-datosMapa <- read.csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQVo0EJNeZWoF4XruYd0BnoHyWxgInurmI3iAWJRZplmYF7_YzGvesj1onZdfY3dbHI6-FRHkuQYXon/pub?gid=905422202&single=true&output=csv')
-datosUltimos7dias <- read.csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQVo0EJNeZWoF4XruYd0BnoHyWxgInurmI3iAWJRZplmYF7_YzGvesj1onZdfY3dbHI6-FRHkuQYXon/pub?gid=302214376&single=true&output=csv')
+datosContagiosHistograma <- read.csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQVo0EJNeZWoF4XruYd0BnoHyWxgInurmI3iAWJRZplmYF7_YzGvesj1onZdfY3dbHI6-FRHkuQYXon/pub?gid=618971894&single=true&output=csv', encoding = 'UTF-8')
+datosMuertesHistograma <- read.csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQVo0EJNeZWoF4XruYd0BnoHyWxgInurmI3iAWJRZplmYF7_YzGvesj1onZdfY3dbHI6-FRHkuQYXon/pub?gid=939250240&single=true&output=csv', encoding = 'UTF-8')
+datosMapa <- read.csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQVo0EJNeZWoF4XruYd0BnoHyWxgInurmI3iAWJRZplmYF7_YzGvesj1onZdfY3dbHI6-FRHkuQYXon/pub?gid=905422202&single=true&output=csv', encoding = 'UTF-8')
+datosUltimos7dias <- read.csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQVo0EJNeZWoF4XruYd0BnoHyWxgInurmI3iAWJRZplmYF7_YzGvesj1onZdfY3dbHI6-FRHkuQYXon/pub?gid=302214376&single=true&output=csv', encoding = 'UTF-8')
 
 ## En función de la carpeta que nos encontremos, escribimos el setwd de una forma u otra
 ## El setwd siempre tiene que atacar a la carpeta 'widget-portada-bloque-covid'
