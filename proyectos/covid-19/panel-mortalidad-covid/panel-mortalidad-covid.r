3038ba9d9660cc0c5677632036533f649586329b
Carlos Munoz Diaz <carlos10md@gmail.com>
2020-11-19 17:07:09 +0100

    Actualización Rebrotes

--- proyectos/covid-19/panel-mortalidad-covid/panel-mortalidad-covid.r
+++ proyectos/covid-19/panel-mortalidad-covid/panel-mortalidad-covid.r
@@ -3,13 +3,27 @@ library(tidyverse)
 library(lubridate)
 
 ## Recogemos todos los CSV de Google Drive
-fallecidosOficial <- read.csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQAbGmq1q9TYkM6Pi4ftN595EBTKLhk6oSR_lA3l_indS_zfYyV7TeFuNv2sGSHX0aIWAx9cE0INzQC/pub?gid=158402147&single=true&output=csv', encoding = 'UTF-8')
-fallecidosMinisterioyCCAA <- read.csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQAbGmq1q9TYkM6Pi4ftN595EBTKLhk6oSR_lA3l_indS_zfYyV7TeFuNv2sGSHX0aIWAx9cE0INzQC/pub?gid=1059673971&single=true&output=csv', encoding = 'UTF-8')
-excesoEspanaFallecidos <- read.csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQAbGmq1q9TYkM6Pi4ftN595EBTKLhk6oSR_lA3l_indS_zfYyV7TeFuNv2sGSHX0aIWAx9cE0INzQC/pub?gid=1107905386&single=true&output=csv', encoding = 'UTF-8')
-#excesoEuropaFallecidos <- read.csv()
+#1.1
+fallecidosOficialUltimaSemana <- read.csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQAbGmq1q9TYkM6Pi4ftN595EBTKLhk6oSR_lA3l_indS_zfYyV7TeFuNv2sGSHX0aIWAx9cE0INzQC/pub?gid=2015297896&single=true&output=csv', encoding = 'UTF-8')
+#1.2
+fallecidosRetrasosCCAA <- read.csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQAbGmq1q9TYkM6Pi4ftN595EBTKLhk6oSR_lA3l_indS_zfYyV7TeFuNv2sGSHX0aIWAx9cE0INzQC/pub?gid=158402147&single=true&output=csv', encoding = 'UTF-8')
+#1.3
+fallecidosOficialCCAA <- read.csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQAbGmq1q9TYkM6Pi4ftN595EBTKLhk6oSR_lA3l_indS_zfYyV7TeFuNv2sGSHX0aIWAx9cE0INzQC/pub?gid=2142056583&single=true&output=csv', encoding = 'UTF-8')
+#1.4
+fallecidosComparativaCCAA <- read.csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQAbGmq1q9TYkM6Pi4ftN595EBTKLhk6oSR_lA3l_indS_zfYyV7TeFuNv2sGSHX0aIWAx9cE0INzQC/pub?gid=1059673971&single=true&output=csv', encoding = 'UTF-8')
+#2.1
+fallecidosExcesoNacional <- read.csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQAbGmq1q9TYkM6Pi4ftN595EBTKLhk6oSR_lA3l_indS_zfYyV7TeFuNv2sGSHX0aIWAx9cE0INzQC/pub?gid=1107905386&single=true&output=csv', encoding = 'UTF-8')
+#2.2
+fallecidosExcesoCCAA <- read.csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQAbGmq1q9TYkM6Pi4ftN595EBTKLhk6oSR_lA3l_indS_zfYyV7TeFuNv2sGSHX0aIWAx9cE0INzQC/pub?gid=968035202&single=true&output=csv', encoding = 'UTF-8')
+#2.3
+fallecidosExcesoAnios <- read.csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQAbGmq1q9TYkM6Pi4ftN595EBTKLhk6oSR_lA3l_indS_zfYyV7TeFuNv2sGSHX0aIWAx9cE0INzQC/pub?gid=65140418&single=true&output=csv', encoding = 'UTF-8')
+#2.4
+fallecidosExcesoOtrosPaises <- read.csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQAbGmq1q9TYkM6Pi4ftN595EBTKLhk6oSR_lA3l_indS_zfYyV7TeFuNv2sGSHX0aIWAx9cE0INzQC/pub?gid=5204283&single=true&output=csv', encoding = 'UTF-8')
+#3.1
 fallecidosEdad <- read.csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQAbGmq1q9TYkM6Pi4ftN595EBTKLhk6oSR_lA3l_indS_zfYyV7TeFuNv2sGSHX0aIWAx9cE0INzQC/pub?gid=1214378008&single=true&output=csv', encoding = 'UTF-8')
-#fallecidosSexo <- read.csv()
-sumaSemanalFallecidos <- read.csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQAbGmq1q9TYkM6Pi4ftN595EBTKLhk6oSR_lA3l_indS_zfYyV7TeFuNv2sGSHX0aIWAx9cE0INzQC/pub?gid=976155095&single=true&output=csv', encoding = 'UTF-8')
+
+####################
+####################
 
 ## En función de la carpeta que nos encontremos, escribimos el setwd de una forma u otra
 ## Descomentar la siguiente línea si estás en la carpeta 'unidad-de-datos'
@@ -17,11 +31,16 @@ setwd('./proyectos/covid-19/panel-mortalidad-covid')
 ## Descomentar la siguiente línea si estás en alguna de las siguientes carpetas: 'brotes-provincias-paises', 'evolucion-datos-covid' o 'widget-portada-bloque-covid'
 #setwd('../panel-mortalidad-covid')
 
+####################
+####################
+
 ## Los mismo datos que hemos recogido (en la actualidad no necesitan ningún proceso de modificación de propiedades), los enviamos a Github
-write_csv(fallecidosOficial, 'fallecidos-oficial-covid.csv')
-write_csv(fallecidosMinisterioyCCAA, 'fallecidos-ccaa-vs-ministerio-covid.csv')
-write_csv(excesoEspanaFallecidos, 'fallecidos-exceso-espana-covid.csv')
-#write_csv(excesoEuropaFallecidos, 'exceso-europa-fallecidos-covid.csv')
-write_csv(fallecidosEdad, 'fallecidos-edad-covid.csv')
-#write_csv(fallecidosSexo, 'fallecidos-sexo-covid.csv')
-write_csv(sumaSemanalFallecidos, 'fallecidos-suma-semanal-covid.csv')
+write.csv(fallecidosOficialUltimaSemana, 'fallecidos-oficial-ultima-semana.csv')
+write.csv(fallecidosRetrasosCCAA, 'fallecidos-retrasos-ccaa.csv')
+write.csv(fallecidosOficialCCAA, 'fallecidos-oficial-ccaa.csv')
+write.csv(fallecidosComparativaCCAA, 'fallecidos-comparativa-ccaa.csv')
+write.csv(fallecidosExcesoNacional, 'fallecidos-exceso-nacional.csv')
+write.csv(fallecidosExcesoCCAA, 'fallecidos-exceso-ccaa.csv')
+write.csv(fallecidosExcesoAnios, 'fallecidos-exceso-anios.csv')
+write.csv(fallecidosExcesoOtrosPaises, 'fallecidos-exceso-otros-paises.csv')
+write.csv(fallecidosEdad, 'fallecidos-edad.csv')
--- proyectos/covid-19/widget-portada-bloque-covid/widget-portada-bloque-covid.r
+++ proyectos/covid-19/widget-portada-bloque-covid/widget-portada-bloque-covid.r
@@ -3,7 +3,7 @@ library(tidyverse)
 library(lubridate)
 
 ## Recogemos todos los CSV de Google Drive
-datosContagiosHistograma <- read.csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQVo0EJNeZWoF4XruYd0BnoHyWxgInurmI3iAWJRZplmYF7_YzGvesj1onZdfY3dbHI6-FRHkuQYXon/pub?gid=618971894&single=true&output=csv', encoding = 'UTF-8')
+datosContagiosHistograma <- read.csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQVo0EJNeZWoF4XruYd0BnoHyWxgInurmI3iAWJRZplmYF7_YzGvesj1onZdfY3dbHI6-FRHkuQYXon/pub?gid=618971894&single=true&output=csv')
 datosMuertesHistograma <- read.csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQVo0EJNeZWoF4XruYd0BnoHyWxgInurmI3iAWJRZplmYF7_YzGvesj1onZdfY3dbHI6-FRHkuQYXon/pub?gid=939250240&single=true&output=csv', encoding = 'UTF-8')
 datosMapa <- read.csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQVo0EJNeZWoF4XruYd0BnoHyWxgInurmI3iAWJRZplmYF7_YzGvesj1onZdfY3dbHI6-FRHkuQYXon/pub?gid=905422202&single=true&output=csv', encoding = 'UTF-8')
 datosUltimos7dias <- read.csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQVo0EJNeZWoF4XruYd0BnoHyWxgInurmI3iAWJRZplmYF7_YzGvesj1onZdfY3dbHI6-FRHkuQYXon/pub?gid=302214376&single=true&output=csv', encoding = 'UTF-8')
