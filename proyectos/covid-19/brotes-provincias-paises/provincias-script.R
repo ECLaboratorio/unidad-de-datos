ce092581ce068c90d852926013b0f435621ab47f
Carlos Munoz Diaz <carlos10md@gmail.com>
2020-12-02 16:57:06 +0100

    Nuevas propiedades para futura actualización de datos provinciales en Rebrotes

--- proyectos/covid-19/brotes-provincias-paises/provincias-script.R
+++ proyectos/covid-19/brotes-provincias-paises/provincias-script.R
@@ -7,7 +7,7 @@ datos <- read.csv("https://cnecovid.isciii.es/covid19/resources/datos_provincias
 datos$fecha <- ymd(datos$fecha)
 
 ## establecer carpeta predefinida
-setwd("proyectos/covid-19/brotes-provincias-paises/")
+#setwd("proyectos/covid-19/brotes-provincias-paises/")
 #setwd("../brotes-provincias-paises")
 
 ## importar nombre y población de las provincias
@@ -27,7 +27,7 @@ rm(provincias)
 
 ## simplificar data frame con columnas de fecha, iso, provincia, población y pcr
 datos_pcr <- datos_provincias %>% 
-    select(fecha, iso, provincia, poblacion, pcr=num_casos_prueba_pcr)
+    select(fecha, iso, provincia, poblacion, pcr=num_casos_prueba_pcr, otras_pruebas=num_casos_prueba_otras)
 
 rm(datos_provincias)
 
@@ -36,26 +36,31 @@ rm(datos_provincias)
 datos_pcr_espana <- datos_pcr %>% 
     group_by(fecha) %>% 
     summarise(poblacion=sum(poblacion),
-              pcr=sum(pcr)) %>% 
+              pcr=sum(pcr),
+              otras_pruebas=sum(otras_pruebas)) %>% 
     mutate(idpro="ES",
            provincia="España",
+           suma_pruebas=pcr+otras_pruebas,
+           total_pruebas_tasa_poblacion=round(suma_pruebas/poblacion*100000, 2),
            pcr_media_movil_7dias=round(rollmean(pcr, k=7, fill=NA, align = "right"),1),
            pcr_7dias=rollsum(pcr, k=7, fill=NA, align = "right"),
            ia_7dias=round(pcr_7dias/poblacion*100000, 2)) %>% 
     filter(fecha>="2020-02-24" & fecha <= "2020-11-26") %>% 
-    select(fecha, idpro, provincia, pcr_diario=pcr, pcr_media_movil_7dias, ia_7dias) %>% 
-    pivot_longer(4:6, names_to="indicador", values_to = "valor") %>% 
+    select(fecha, idpro, provincia, pcr_media_movil_7dias, ia_7dias, pcr_diario=pcr, otras_pruebas_diarias=otras_pruebas, suma_pruebas, total_pruebas_tasa_poblacion) %>% 
+    pivot_longer(4:9, names_to="indicador", values_to = "valor") %>% 
     arrange(fecha, idpro)
 
 
 ## calcular media móvil de 7 días de casos diarios, casos acumulados en 7 días e IA 7 días para cada provincia
 datos_pcr_provincias <- datos_pcr %>% 
     mutate(pcr_media_movil_7dias=round(rollmean(pcr, k=7, fill=NA, align = "right"),1),
+           suma_pruebas=pcr+otras_pruebas,
+           total_pruebas_tasa_poblacion=round(suma_pruebas/poblacion*100000, 2),
            pcr_7dias=rollsum(pcr, k=7, fill=NA, align = "right"),
            ia_7dias=round(pcr_7dias/poblacion*100000, 2)) %>% 
     filter(fecha>="2020-02-24" & fecha <= "2020-11-26") %>%
-    select(fecha, idpro=iso, provincia, pcr_diario=pcr, pcr_media_movil_7dias, ia_7dias) %>% 
-    pivot_longer(4:6, names_to="indicador", values_to = "valor")
+    select(fecha, idpro=iso, provincia, pcr_media_movil_7dias, ia_7dias, pcr_diario=pcr, otras_pruebas_diarias=otras_pruebas, suma_pruebas, total_pruebas_tasa_poblacion) %>% 
+    pivot_longer(4:9, names_to="indicador", values_to = "valor")
 
 
 ## unir data frames de España y provincias en uno único y guardar el archivo en formato tsv
